940ef177cc95e8e1ad0e60a4760f645aac400eef
waki285 <suzuneu@suzuneu.com>
2024-06-20 16:20:32 +0900

    test

--- src/tasks_onetime/bt1.rs
+++ src/tasks_onetime/bt1.rs
@@ -4,7 +4,27 @@ use regex::Regex;
 use crate::util::summary;
 
 pub async fn bt1(bot: &mwbot::Bot) -> Result<(), Box<dyn std::error::Error>> {
-    
+    let list = include_str!("./list.txt");
+
+    let list = list.lines().collect::<Vec<_>>();
+
+    let regex = Regex::new(r"(?i)\[\[Category:SRE[ _]guidelines[_ ]and[ _]guides\]\]")?;
+
+    for title in list {
+        let title = title.trim();
+        let page = bot.page(title)?;
+        let text = page.wikitext().await?;
+
+        let text = regex.replace_all(&text, "[[Category:Technology guidelines and guides]]");
+
+        //dbg!(&text);
+
+        page.save(text.to_string(), &SaveOptions::summary(&summary(&format!("Onetime Task ([[User:Waki285-Bot/tasks/BT1]]) Replace redirect category"))).mark_as_bot(false)).await.ok();
+
+        println!("Edited: {}", title);
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+    }
 
     Ok(())
 }
